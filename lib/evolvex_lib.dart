478999b27295b8f040fc069433ab7f08a880a95e
Surya Manivannan <suryamanivannan@Suryas-MacBook-Pro.local>
2023-11-30 21:48:55 +0530

    comments added

--- lib/evolvex_lib.dart
+++ lib/evolvex_lib.dart
@@ -1,23 +1,33 @@
 library evolvex_lib;
 
+// BLOC
 export 'src/state_management/bloc/app_bloc_observer.dart';
 export 'src/state_management/bloc/base_bloc.dart';
 
+// CUBIT
 export 'src/state_management/cubit/base_cubit.dart';
+
+// MVVM - PROVIDER
 export 'src/state_management/mvvm/view_model.dart';
 export 'src/state_management/mvvm/view_model_widget.dart';
 
+// ERROR HANDLING
 export 'src/error/failure.dart';
 export 'src/error/exception_handler.dart';
 
+// DATA 
 export 'src/resources/data_state.dart';
 
+// USECASE
 export 'src/usecase/usecase.dart';
 
+// UTILS 
 export 'src/helpers/utils.dart';
 export 'src/helpers/styles.dart';
 
+// SERVICE
 export 'src/service/api_service.dart';
 export 'src/service/navigation_service.dart';
 
+// MAIN
 export 'src/evolvex_main.dart';
