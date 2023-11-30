b340487445f585ebc16ba953a10ef2d439b5adf5
Surya Manivannan <suryamanivannan@Suryas-MacBook-Pro.local>
2023-11-30 22:08:46 +0530

    viewmodelwidget updated

--- lib/src/state_management/mvvm/view_model_widget.dart
+++ lib/src/state_management/mvvm/view_model_widget.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
 import 'view_model.dart';
 
-abstract class ViewModelWidget<T extends ViewModel> extends StatelessWidget {
+abstract class ViewModelWidget<T extends ViewModel> extends HookWidget {
   const ViewModelWidget({super.key});
 
   Widget buildWidget(BuildContext context, T viewModel);
+
+  Future<void> init(T viewModel);
 
   @override
   Widget build(BuildContext context) {
