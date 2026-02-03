4765461ee3fdf2fb4ec0a60ffddb02ac8ec44232
Surya Manivannan <suryamanivannan@Suryas-MacBook-Pro.local>
2026-02-03 21:46:41 +0530

    new try

--- lib/src/evolvex_main.dart
+++ lib/src/evolvex_main.dart
@@ -1,15 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_loggy/flutter_loggy.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loggy/loggy.dart';
 
 import 'state_management/bloc/app_bloc_observer.dart';
 
-
 class EvolveX {
-  static init() {
+  late final ScreenUtilInit screenUtilInitMaterialApp;
+  void init({bool isBlocStateManagement = false}) {
+    screenUtilInitMaterialApp = ScreenUtilInit(minTextAdapt: true);
     Loggy.initLoggy(
       logPrinter: const PrettyDeveloperPrinter(),
     );
-    Bloc.observer = AppBlocObserver();
+    if (isBlocStateManagement) {
+      Bloc.observer = AppBlocObserver();
+    }
   }
 }
